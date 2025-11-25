62b1f734b30d63336b7787ab6466298e905fd373
paridhiramtani <ramtaniparidhi@gmail.com>
2025-11-26 02:21:20 +0530

    Update service.js

--- public/service.js
+++ public/service.js
@@ -1,1 +1,21 @@
+var app = angular.module("HobbyHubApp");
 
+app.service('apiService', ['$http', function($http) {
+  const API_URL = 'http://localhost:5000/api';
+
+  this.register = function(userData) {
+    return $http.post(API_URL + '/register', userData);
+  };
+
+  this.login = function(credentials) {
+    return $http.post(API_URL + '/login', credentials);
+  };
+
+  this.getMessages = function(hobby) {
+    return $http.get(API_URL + '/messages/' + hobby);
+  };
+
+  this.getCountryCodes = function() {
+    return $http.get('https://restcountries.com/v3.1/all');
+  };
+}]);
